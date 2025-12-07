ea2b178c8032ee12c40e3ec8a414630bd41e1cd7
etranger2268 <shun.5.2268@gmail.com>
2025-12-07 23:46:28 +0900

    develop-4-23

--- src/main.js
+++ src/main.js
@@ -1,57 +1,28 @@
-/*
-  var変数
-*/
-var val1 = 'var変数';
-console.log(val1);
+// 従来の関数
+function func1(str) {
+  return str;
+}
+console.log(func1('func1です'));
 
-// var変数は上書き可能
-val1 = 'var変数を上書き';
-console.log(val1);
+// 関数式
+const func2 = function(str) {
+  return str;
+}
+console.log(func2('func2です'));
 
-// var変数は再宣言可能
-var val1 = 'var変数を再宣言';
-console.log(val1);
-
-/*
-  let変数
-*/
-let val2 = 'let変数';
-console.log(val2);
-
-// let変数は上書き可能
-val2 = 'let変数を上書き';
-console.log(val2);
-
-// let変数は再宣言不可
-// let val2 = 'let変数を再宣言';
-
-/*
-  const変数
-*/
-const val3 = 'const変数';
-console.log(val3);
-
-// const変数は上書き不可
-// val3 = 'const変数を上書き';
-
-// const変数は再宣言不可
-// const val3 = 'const変数を再宣言';
-
-// constで定義したオブジェクトはプロパティの変更が可能
-const val4 = {
-  name: 'etranger2268',
-  age: 26,
+// アロー関数
+const func3 = str => {
+  return str;
 };
-console.log(val4);
+console.log(func3('func3です'));
 
-val4.name = 'etranger';
-val4.age = 6;
-console.log(val4);
+const func4 = str => str;
+console.log(func4('func4です'));
 
-// constで定義した配列はプロパティの変更が可能
-const val5 = ['red', 'yellow'];
-console.log(val5);
+// example
+const sum = (num1, num2) => num1 + num2;
+console.log(sum(10, 20));
 
-val5[0] = 'blue';
-val5.push('green');
-console.log(val5);
+// return object
+const createUser = (str, num) => ({ name: str, age: num });
+console.log(createUser('etranger2268', 26));
