f59ba651c1ba8208426aa6858dda804a99f81c1d
etranger2268 <shun.5.2268@gmail.com>
2025-12-10 16:19:17 +0900

    develop-7-54

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,3 +1,38 @@
 export default function Todo() {
-  return <h1>Hello, Todo-list</h1>;
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="TODOを入力" />
+        <button type="button">追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODOです</p>
+            <button type="button">完了</button>
+            <button type="button">削除</button>
+          </li>
+          <li>
+            <p>TODOです</p>
+            <button type="button">完了</button>
+            <button type="button">削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODOでした</p>
+            <button type="button">戻す</button>
+          </li>
+          <li>
+            <p>TODOでした</p>
+            <button type="button">戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
