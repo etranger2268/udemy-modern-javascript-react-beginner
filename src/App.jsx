af7eed6423821776652a99545f46faa264278b7b
etranger2268 <shun.5.2268@gmail.com>
2025-12-10 14:56:28 +0900

    develop-6-49

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ColorfulMessage from './components/ColorfulMessage';
 
 export default function App() {
   const [num, setNum] = useState(0);
+  const [isShowFace, setIsShowFace] = useState(true);
   const onClickButton = () => {
     setNum((prev) => prev + 1);
-    setNum((prev) => prev + 1);
   };
+
+  const onClickToggle = () => setIsShowFace((prev) => !prev);
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Do not include "isShowFace" in the dependency list
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        isShowFace || setIsShowFace(true);
+      } else {
+        isShowFace && setIsShowFace(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
@@ -17,6 +30,10 @@ export default function App() {
         ボタン
       </button>
       <p>{num}</p>
+      <button type="button" onClick={onClickToggle}>
+        on/off
+      </button>
+      {isShowFace && <p>( *´艸｀)</p>}
     </>
   );
 }
