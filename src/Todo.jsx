f1ace2c901034a919dc20ef2be70175ac974cce5
etranger2268 <shun.5.2268@gmail.com>
2025-12-11 16:37:17 +0900

    develop-7-60

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -22,6 +22,11 @@ export default function Todo() {
     setCompleteTodos((prev) => [...prev, incompleteTodos[index]]);
   };
 
+  const onClickBack = (index) => {
+    setCompleteTodos((prev) => prev.filter((_, idx) => idx !== index));
+    setIncompleteTodos((prev) => [...prev, completeTodos[index]]);
+  };
+
   return (
     <>
       <div className="m-2 max-w-md rounded bg-green-200 p-2">
@@ -69,12 +74,13 @@ export default function Todo() {
       <div className="m-2 min-h-52 max-w-md rounded-xl border border-green-500 bg-green-100 p-2">
         <p className="mt-0 mb-3 text-center font-black">完了のTODO</p>
         <ul className="ml-6 list-disc">
-          {completeTodos.map((todo) => (
+          {completeTodos.map((todo, index) => (
             <li key={todo}>
               <div className="flex items-center space-x-1">
                 <p class="m-1.5">{todo}</p>
                 <button
                   type="button"
+                  onClick={() => onClickBack(index)}
                   className="rounded-xl bg-gray-100 px-4 py-1 hover:cursor-pointer hover:bg-gray-200 hover:text-white"
                 >
                   戻す
