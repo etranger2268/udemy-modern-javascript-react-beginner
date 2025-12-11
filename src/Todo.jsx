075dba6dc4815a61ad732c5251294cd3798a6ff5
etranger2268 <shun.5.2268@gmail.com>
2025-12-11 16:31:41 +0900

    develop-7-58

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -14,6 +14,9 @@ export default function Todo() {
     setTodoText('');
   }
 
+  const onClickDelete = (index) =>
+      setIncompleteTodos(prev => prev.filter((_,idx) => idx !== index))
+
   return (
     <>
       <div className="m-2 max-w-md rounded bg-green-200 p-2">
@@ -36,7 +39,7 @@ export default function Todo() {
       <div className="m-2 min-h-52 max-w-md rounded-xl border border-green-500 p-2">
         <p className="mt-0 mb-3 text-center font-black">未完了のTODO</p>
         <ul className="ml-6 list-disc">
-          {incompleteTodos.map((todo) => (
+          {incompleteTodos.map((todo, index) => (
             <li key={todo}>
               <div className="flex items-center space-x-1">
                 <p class="m-1.5">{todo}</p>
@@ -48,6 +51,7 @@ export default function Todo() {
                 </button>
                 <button
                   type="button"
+                  onClick={() => onClickDelete(index)}
                   className="rounded-xl bg-gray-100 px-4 py-1 hover:cursor-pointer hover:bg-gray-200 hover:text-white"
                 >
                   削除
